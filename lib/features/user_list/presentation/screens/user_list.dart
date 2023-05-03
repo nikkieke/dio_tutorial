41fbcaec42b2c7ceaab37998f2e65cb71989e0f7
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-05-03 03:26:31 +0100

    create user_list file

---
+++ lib/features/user_list/presentation/screens/user_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UserList extends StatelessWidget {
+  const UserList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
